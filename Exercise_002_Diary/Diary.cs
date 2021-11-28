5fc2a3a55852f1b9876d9767ddc425cc0c34f89f
Dmitriy Balkanov <dmitriybalkanov@yandex.ru>
2021-11-28 21:22:14 +0800

    Корректировка 1 по замечаниям. Вытащил интерфейс из структуры, поместил в Main. Добавил примитивный отлов ошибок. Добавил сообщение об успешном выполнении (или неуспешном) команды.

--- Exercise_002_Diary/Diary.cs
+++ Exercise_002_Diary/Diary.cs
@@ -31,12 +31,6 @@ namespace Exercise_002_Diary
         private Record[] records;
         // Путь к файлу ежедневника
         private string path;
-        //// Путь к файлу для импорта в ежедневник
-        //private string uPath;
-        ///// <summary>
-        ///// Путь к файлу для импорта
-        ///// </summary>
-        //public string UPath { set { uPath = value; } }
         
         // Индекс текущего элемента массива для добавления
         private int index;
@@ -48,11 +42,15 @@ namespace Exercise_002_Diary
         public Diary(string Path)
         {
             this.path = Path;
-            //this.uPath = string.Empty;
             this.records = new Record[1];
             this.index = 0;
-            this.Load();
+            this.Load(Path);
         }
+
+        /// <summary>
+        /// Количество записей в дневнике
+        /// </summary>
+        public int Count { get { return this.index; } }
 
         /// <summary>
         /// Добавление записи в ежедневник
@@ -66,39 +64,19 @@ namespace Exercise_002_Diary
         }
 
         /// <summary>
-        /// Пользовательский ввод для добавления или изменения записи
-        /// </summary>
-        /// <returns></returns>
-        private string[] GetUserData()
-        {
-            string[] args = new string[4];
-
-            Console.WriteLine("Введите заголовок записи:");
-            args[0] = Console.ReadLine(); // title
-            Console.WriteLine();
-            Console.WriteLine("Введите дату /дд.мм.гггг чч:мм/ (пустое поле - сегодня)");
-            args[1] = Console.ReadLine(); // datetime
-            if (args[1].Length == 0) args[1] = DateTime.Now.ToString("g");
-            Console.WriteLine();
-            Console.WriteLine("Введите текст записи:");
-            args[2] = Console.ReadLine(); // task
-            Console.WriteLine();
-            Console.WriteLine("Введите контакт:");
-            args[3] = Console.ReadLine(); // contact
-
-            return args;
-        }
-
-        /// <summary>
         /// Создание записи в ежедневнике вручную
         /// </summary>
-        public void Create()
+        public bool Create(string title, string dateTime, string task, string contact)
         {
-            Console.Clear();
-            Console.WriteLine("Добавить запись в ежедневник");
-            Console.WriteLine();
-            string[] args = GetUserData();
-            Add(new Record(args[0], DateTime.Parse(args[1]), args[2], args[3]));
+            try
+            {
+                Add(new Record(title, DateTime.Parse(dateTime), task, contact));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
         /// <summary>
@@ -106,67 +84,79 @@ namespace Exercise_002_Diary
         /// </summary>
         /// <param name="rec">Ежедневник</param>
         /// <param name="index">Индекс записи для удаления</param>
-        public void Delete(int index)
+        public bool Delete(int index)
         {
-            Record[] temp = new Record[this.records.Length - 1];
-            for (int i = 0; i < index; i++)
+            try
             {
-                temp[i] = this.records[i];
+                Record[] temp = new Record[this.records.Length - 1];
+                for (int i = 0; i < index; i++)
+                {
+                    temp[i] = this.records[i];
+                }
+                for (int i = index + 1; i < temp.Length; i++)
+                {
+                    temp[i - 1] = this.records[i];
+                }
+                this.records = temp;
+                this.index--;
+                return true;
             }
-            for (int i = index + 1; i < temp.Length; i++)
+            catch (Exception ex)
             {
-                temp[i - 1] = this.records[i];
+                return false;
             }
-            this.records = temp;
-            this.index--;
         }
 
         /// <summary>
         /// Редактирование записи в ежедневнике
         /// </summary>
         /// <param name="index"></param>
-        public void Edit(int index)
+        public bool Update(int index, string title, string dateTime, string task, string contact)
         {
-            Console.Clear();
-            Console.WriteLine("Редактирование записи");
+            try
+            {
+                for (int i = 0; i < this.index; i++)
+                    if (this.records[i].RecordId == index) index = i;
 
-            for (int i = 0; i < this.index; i++)
-                if (this.records[i].RecordId == index) index = i;
-                
-            ref Record rec = ref this.records[index];
+                ref Record rec = ref this.records[index];
 
-            string[] args = GetUserData();
-
-            rec.Title = (args[0] == string.Empty) ? rec.Title : args[0];
-            rec.DateAndTime = (args[1] == string.Empty) ? rec.DateAndTime : DateTime.Parse(args[1]);
-            rec.Task = (args[2] == string.Empty) ? rec.Task : args[2];
-            rec.Contact = (args[3] == string.Empty) ? rec.Contact : args[3];
+                if (title != string.Empty) rec.Title = title;
+                if (dateTime != string.Empty) rec.DateAndTime = DateTime.Parse(dateTime);
+                if (task != string.Empty) rec.Task = task;
+                if (contact != string.Empty) rec.Contact = contact;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
-        /// <summary>
-        /// Загрузка данных из файла по дефолтному пути
-        /// </summary>
-        public void Load()
-        {
-            Load(this.path);
-        }
         /// <summary>
         /// Загрузка данных из файла по указанному пути
         /// </summary>
         /// <param name="path">Путь к файлу данных для импорта</param>
-        public void Load(string path)
+        public bool Load(string path)
         {
-            if (!IsExistedFile(path))
+            try
             {
-                File.Create(path);
-            }
-            using(StreamReader sr = new StreamReader(path))
-            {
-                while (!sr.EndOfStream)
+                if (!IsExistedFile(path))
                 {
-                    string[] args = sr.ReadLine().Split(';');
-                    Add(new Record(args[0], DateTime.Parse(args[1]), args[2], args[3]));
+                    File.Create(path).Close();
                 }
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] args = sr.ReadLine().Split(';');
+                        Add(new Record(args[0], DateTime.Parse(args[1]), args[2], args[3]));
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
             }
         }
         /// <summary>
@@ -175,48 +165,63 @@ namespace Exercise_002_Diary
         /// <param name="path">Путь к файлу для импорта</param>
         /// <param name="start">Начало диапазона дат для импора</param>
         /// <param name="end">Конец диапазона дат для импорта</param>
-        public void Load(string path, DateTime start, DateTime end)
+        public bool Load(string path, string start, string end)
         {
-            using (StreamReader sr = new StreamReader(path))
+            try
             {
-                while (!sr.EndOfStream)
+                if (!IsExistedFile(path))
                 {
-                    string[] args = sr.ReadLine().Split(';');
-                    if (DateTime.Parse(args[1]) >= start && DateTime.Parse(args[1]) <= end)
-                    {
-                        Add(new Record(args[0], DateTime.Parse(args[1]), args[2], args[3]));
-                    }
+                    File.Create(path).Close();
                 }
 
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] args = sr.ReadLine().Split(';');
+                        if (DateTime.Parse(args[1]) >= DateTime.Parse(start) &&
+                            DateTime.Parse(args[1]) <= DateTime.Parse(end))
+                        {
+                            Add(new Record(args[0], DateTime.Parse(args[1]), args[2], args[3]));
+                        }
+                    }
+
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
             }
         }
 
         /// <summary>
-        /// Сохранение данных в файл по дефолтному пути
-        /// </summary>
-        public void Save()
-        {
-            Save(this.path);
-        }
-        /// <summary>
         /// Сохранение данных в файл по указанному пути
         /// </summary>
         /// <param name="path">Путь к файлу для сохранения данных</param>
-        public void Save(string path)
+        public bool Save()
         {
-            using (StreamWriter sw = new StreamWriter(path))
+            try
             {
-                for (int i = 0; i < this.index; i++)
+                using (StreamWriter sw = new StreamWriter(this.path))
                 {
-                    Record rec = this.records[i];
+                    for (int i = 0; i < this.index; i++)
+                    {
+                        Record rec = this.records[i];
 
-                    string temp = String.Format("{0};{1};{2};{3}", 
-                        rec.Title, 
-                        rec.DateAndTime.ToString("g"), 
-                        rec.Task, 
-                        rec.Contact);
-                    sw.WriteLine(temp, (i == 0));
+                        string temp = String.Format("{0};{1};{2};{3}",
+                            rec.Title,
+                            rec.DateAndTime.ToString("g"),
+                            rec.Task,
+                            rec.Contact);
+                        sw.WriteLine(temp, (i == 0));
+                    }
                 }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
             }
         }
 
@@ -237,32 +242,41 @@ namespace Exercise_002_Diary
         /// Сортировка
         /// </summary>
         /// <param name="numField">Условный номер поля. 1-ID, 2-заголовок, 3-дата, 4-контакт</param>
-        public void Sort(int numField)
+        public bool Sort(int numField)
         {
-            for (int i = 1; i < this.index; i++)
+            try
             {
-                for (int j = 0; j < this.index - i; j++)
+                for (int i = 1; i < this.index; i++)
                 {
-                    switch (numField)
+                    for (int j = 0; j < this.index - i; j++)
                     {
-                        case 1:
-                            if (this.records[j].RecordId > this.records[j + 1].RecordId)
-                                Swap(ref this.records[j], ref this.records[j + 1]);
-                            break;
-                        case 2:
-                            if (String.Compare(this.records[j].Title, this.records[j + 1].Title) > 0)
-                                Swap(ref this.records[j], ref this.records[j + 1]);
-                            break;
-                        case 3:
-                            if (this.records[j].DateAndTime > this.records[j + 1].DateAndTime)
-                                Swap(ref this.records[j], ref this.records[j + 1]);
-                            break;
-                        default:
-                            if (String.Compare(this.records[j].Contact, this.records[j + 1].Contact) > 0)
-                                Swap(ref this.records[j], ref this.records[j + 1]);
-                            break;
+                        switch (numField)
+                        {
+                            case 1:
+                                if (this.records[j].RecordId > this.records[j + 1].RecordId)
+                                    Swap(ref this.records[j], ref this.records[j + 1]);
+                                break;
+                            case 2:
+                                if (String.Compare(this.records[j].Title, this.records[j + 1].Title) > 0)
+                                    Swap(ref this.records[j], ref this.records[j + 1]);
+                                break;
+                            case 3:
+                                if (this.records[j].DateAndTime > this.records[j + 1].DateAndTime)
+                                    Swap(ref this.records[j], ref this.records[j + 1]);
+                                break;
+                            default:
+                                if (String.Compare(this.records[j].Contact, this.records[j + 1].Contact) > 0)
+                                    Swap(ref this.records[j], ref this.records[j + 1]);
+                                break;
+                        }
                     }
                 }
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
             }
         }
 
@@ -284,6 +298,22 @@ namespace Exercise_002_Diary
         {
             return File.Exists(path);
         }
+
+        public void Print()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine("{0}   {1}   {2}   {3}   {4}",
+                    this.records[i].RecordId,
+                    this.records[i].Title,
+                    this.records[i].DateAndTime.ToString("g"),
+                    this.records[i].Task,
+                    this.records[i].Contact);
+            }
+        }
+
+
+        
         
     }
 }
--- Exercise_002_Diary/Program.cs
+++ Exercise_002_Diary/Program.cs
@@ -30,16 +30,207 @@ namespace Exercise_002_Diary
 
         static void Main(string[] args)
         {
-            Diary diary = new Diary(@"D:\Diary.txt");
-            //for (int i = 0; i < 5; i++)
-            //    diary.Create();
-            //diary.Save();
-            //diary.Delete(1);
-            //diary.Sort(3);
-            //diary.Save();
-            //diary.Edit(6);
-            //diary.Save();
+            string mainPath = @"D:\Diary.txt";
+
+            Diary diary = new Diary(mainPath);
+
+            Console.WriteLine("-- == Е Ж Е Д Н Е В Н И К == --");
+            Console.WriteLine();
+            Console.WriteLine($"Фаил дневника:         {mainPath}");
+            Console.WriteLine($"Записей в дневнике:    {diary.Count}");
+            Console.WriteLine();
+
+            Prompt(ref diary);
+        }
+        /// <summary>
+        /// Приглашение пользователя к дальнейшим действиям
+        /// </summary>
+        /// <param name="diary"></param>
+        static void Prompt(ref Diary diary)
+        {
+            Console.WriteLine("Выберите дальнейшее действие:");
+            Console.WriteLine("1 - Создать запись вручную");
+            if (diary.Count != 0) Console.WriteLine("2 - Удалить запись");
+            if (diary.Count != 0) Console.WriteLine("3 - Изменить запись");
+            if (diary.Count > 1) Console.WriteLine("4 - Упорядочить записи в дневнике");
+            Console.WriteLine("5 - Импорт записей из файла");
+            Console.WriteLine("6 - Сохранить дневник в файл");
+            if (diary.Count != 0) Console.WriteLine("7 - Вывести содержимое дневника в консоль");
+
+            do
+            {
+                string key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        CreateRecord(ref diary);
+                        break;
+                    case "2":
+                        DeleteRecord(ref diary);
+                        break;
+                    case "3":
+                        EditRecord(ref diary);
+                        break;
+                    case "4":
+                        SortRecords(ref diary);
+                        break;
+                    case "5":
+                        ImportFile(ref diary);
+                        break;
+                    case "6":
+                        SaveDiaryToFile(ref diary);
+                        break;
+                    case "7":
+                        Print(ref diary);
+                        break;
+                    default:
+                        continue;
+                }
+
+                break;
+
+            } while (true);
 
         }
+
+
+        static void CreateRecord(ref Diary diary)
+        {
+            Console.WriteLine("Создание записи в ежедневнике");
+            Console.WriteLine("Введите заголовок записи:");
+            string title = Console.ReadLine(); // title
+            Console.WriteLine();
+            Console.WriteLine("Введите дату /дд.мм.гггг чч:мм/ (пустое поле - сегодня)");
+            string datetime = Console.ReadLine(); // datetime
+            if (datetime.Length == 0) datetime = DateTime.Now.ToString("g");
+            Console.WriteLine();
+            Console.WriteLine("Введите текст записи:");
+            string task = Console.ReadLine(); // task
+            Console.WriteLine();
+            Console.WriteLine("Введите контакт:");
+            string contact = Console.ReadLine(); // contact
+
+            Success(diary.Create(title, datetime, task, contact));
+
+            Console.WriteLine();
+            Prompt(ref diary);
+
+        }
+
+        static void DeleteRecord(ref Diary diary)
+        {
+            Console.WriteLine("Удаление записи");
+            Console.Write("Укажите ID записи для удаления:   ");
+            int id = int.Parse(Console.ReadLine());
+            Success(diary.Delete(id));
+
+            Console.WriteLine();
+            Prompt(ref diary);
+
+        }
+
+        static void EditRecord(ref Diary diary)
+        {
+            Console.WriteLine("Редактирование записи в ежедневнике");
+            Console.WriteLine("Введите номер записи для редактирования (оставить пустым - без изменения):");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите заголовок записи:");
+            string title = Console.ReadLine(); // title
+            Console.WriteLine();
+            Console.WriteLine("Введите дату /дд.мм.гггг чч:мм/ (пустое поле - сегодня)");
+            string datetime = Console.ReadLine(); // datetime
+            Console.WriteLine();
+            Console.WriteLine("Введите текст записи:");
+            string task = Console.ReadLine(); // task
+            Console.WriteLine();
+            Console.WriteLine("Введите контакт:");
+            string contact = Console.ReadLine(); // contact
+
+            Success(diary.Update(id, title, datetime, task, contact));
+
+            Console.WriteLine();
+            Prompt(ref diary);
+
+        }
+
+        static void SortRecords(ref Diary diary)
+        {
+            Console.WriteLine("Упорядочивание записей в ежедневнике");
+            Console.Write("Укажите номер поля для упорядочивания:");
+            Console.Write("1 - ID, 2 - Заголовок, 3 - Дата, 4 - Контакт:");
+            int key = int.Parse(Console.ReadLine());
+            diary.Sort(key);
+
+            Console.WriteLine();
+            Prompt(ref diary);
+
+        }
+
+        static void ImportFile(ref Diary diary)
+        {
+            Console.WriteLine("Импорт данных из файла");
+            Console.WriteLine("Укажите путь к файлу для импорта:");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Что импортировать?:");
+            Console.WriteLine("1 - Импорт файла целиком, 2 - импорт по диапазону дат");
+            do
+            {
+                string key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        Success(diary.Load(path));
+                        break;
+                    case "2":
+                        Console.WriteLine("Укажите диапазон дат:");
+                        string date1 = Console.ReadLine();
+                        string date2 = Console.ReadLine();
+                        Success(diary.Load(path, date1, date2));
+                        break;
+                    default:
+                        continue;
+                }
+
+                break;
+
+            } while(true);
+
+            Console.WriteLine();
+            Prompt(ref diary);
+
+        }
+
+        static void SaveDiaryToFile(ref Diary diary)
+        {
+            Console.WriteLine("Сохранение данных в файл");
+            Success(diary.Save());
+            Console.WriteLine();
+            Prompt(ref diary);
+
+        }
+
+        static void Print(ref Diary diary)
+        {
+            Console.WriteLine();
+            diary.Print();
+            Console.WriteLine();
+            Prompt(ref diary);
+        }
+
+        static void Success(bool flag)
+        {
+            if (flag)
+            {
+                Console.WriteLine("Успешно!");
+            }
+            else
+            {
+                Console.WriteLine("Действие не удалось!");
+            }
+
+        }
+
     }
 }
